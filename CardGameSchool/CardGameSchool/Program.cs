0d96bb262988575efd31d058ae9554f1dd8a645c
Michael Bäck <majkieboy@gmail.com>
2021-08-18 17:00:32 +0200

    Just playing with git.

--- CardGameSchool/CardGameSchool/Program.cs
+++ CardGameSchool/CardGameSchool/Program.cs
@@ -6,6 +6,7 @@ namespace CardGameSchool
     {
         static void Main(string[] args)
         {
+            var test = "test git.";
             Console.WriteLine("Just testing git, card game will come later...");
         }
     }
