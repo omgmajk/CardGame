3532a77d43784f69361398812969cdb3f0630dc7
Michael Bäck <majkieboy@gmail.com>
2021-08-21 21:25:29 +0200

    Added more code.

--- CardGameSchool/CardGameSchool/Program.cs
+++ CardGameSchool/CardGameSchool/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CardGameSchool
 {
+
+    public enum Cards
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 14
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var test = "test git.";
-            Console.WriteLine("Just testing git, card game will come later...");
+            Queue<Enum> deck = new Queue<Enum>();
+            deck.Enqueue(Cards.Ace);
+            deck.Enqueue(Cards.Two);
+            deck.Enqueue(Cards.Three);
+            var currentCard = deck.Dequeue();
         }
     }
 }
