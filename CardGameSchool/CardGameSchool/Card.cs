c64f520bb6398b5ba1b517a884c3c10d8e12e5d9
Michael Bäck <majkieboy@gmail.com>
2021-08-23 15:59:56 +0200

    Added GetCard methods.

---
+++ CardGameSchool/CardGameSchool/Card.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CardGameSchool
+{
+    public class Card
+    {
+        private string _name;
+        private Suit _suit;
+        private int _value;
+        private string _shortName;
+
+        public string Name { get => _name; set => _name = value; }
+        public Suit Suit { get => _suit; set => _suit = value; }
+        public int Value { get => _value; set => this._value = value; }
+        public string ShortName { get => _shortName; set => _shortName = value; }
+
+        public static string GetName(int val)
+        {
+            switch (val)
+            {
+                case <= 10: // Must be compiled with .NET 5.0
+                    return val.ToString();
+                case 11:
+                    return "Jack";
+                case 12:
+                    return "Queen";
+                case 13:
+                    return "King";
+                case 14:
+                    return "Ace";
+                default:
+                    return "Something went wrong...";
+            }
+        }
+
+        public static string GetLongName(Card card){
+            if (card.Value > 10)
+                return $"{GetName(card.Value)} of {card.Suit}";
+            else
+                return $"{card.Value} of {card.Suit}";
+        }
+    }
+}
--- CardGameSchool/CardGameSchool/Program.cs
+++ CardGameSchool/CardGameSchool/Program.cs
@@ -4,17 +4,6 @@ namespace CardGameSchool
 {
     // Quick enum to make a difference between suits for deck building. Not needed in the game I ended up with.
     public enum Suit { Diamonds, Clubs, Spades, Hearts }
-    
-    public class Card
-    {
-        private string _name;
-        private Suit _suit;
-        private int _value;
-
-        public string Name { get => _name; set => _name = value; }
-        public Suit Suit { get => _suit; set => _suit = value; }
-        public int Value { get => _value; set => this._value = value; }
-    }
 
     class Program
     {
@@ -25,13 +14,15 @@ namespace CardGameSchool
             aceofspades.Name = "Ace of Spades";
             aceofspades.Suit = Suit.Spades;
             aceofspades.Value = 14;
-            
+            aceofspades.ShortName = "♠A";
+
             // Actually works! Three cards in the deck.
             mike.Deck.Enqueue(aceofspades);
             mike.Deck.Enqueue(aceofspades);
             mike.Deck.Enqueue(aceofspades);
             var test = mike.Deck.Peek();
-            Console.WriteLine(test.Value);
+            Console.WriteLine(Card.GetLongName(aceofspades));
+            Console.WriteLine(test.ShortName);
             Console.WriteLine(mike.Deck.Count);
         }
     }
